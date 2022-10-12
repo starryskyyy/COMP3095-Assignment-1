ec5539acd48afd8d2bab0e35d31393c4765124a9
Hoon <yimsh4507@gmail.com>
2022-10-12 10:54:58 -0400

    Updated Recipe Schema

--- src/main/java/gbc/comp3095/assignment1/UserEntity/Recipe.java
+++ src/main/java/gbc/comp3095/assignment1/UserEntity/Recipe.java
@@ -1,4 +1,24 @@
 package gbc.comp3095.assignment1.UserEntity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "recipes")
 public class Recipe {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String description;
+    private String instruction;
 }
