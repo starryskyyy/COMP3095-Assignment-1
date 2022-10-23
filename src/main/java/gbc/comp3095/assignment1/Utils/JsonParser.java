33fa4d46d1735ec6c60cec5693196424fe07d86a
Hoon Yim <yimsh4507@gmail.com>
2022-10-23 08:24:57 -0400

    Created JsonParser

---
+++ src/main/java/gbc/comp3095/assignment1/Utils/JsonParser.java
@@ -0,0 +1,10 @@
+package gbc.comp3095.assignment1.Utils;
+
+import gbc.comp3095.assignment1.Entity.User;
+import org.json.simple.JSONArray;
+
+import java.util.List;
+
+public class JsonParser {
+    private JSONArray userArray;
+}
